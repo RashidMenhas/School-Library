666c4b714ea5c840d09ad7725b73dd54c6ebca79
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-26 11:48:22 +0500

    The TrimmerDecorator Create a class that inherits from the base Decorator class.Implement a method correct_name that makes sure that the output of @nameable.correct_name has a maximum of 10 characters. If it's longer it should trim the word.

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
@@ -19,5 +22,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
---
+++ trimmerdecorator.rb
@@ -0,0 +1,7 @@
+require './decorator'
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0, 10] : @nameable.correct_name
+  end
+end
