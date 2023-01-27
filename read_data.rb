bec730cae3cabb850a180347ba077b21dbeec23a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-27 22:56:49 +0100

    Add read_people method to ReadData Class

--- read_data.rb
+++ read_data.rb
@@ -13,3 +13,25 @@ class ReadData
     file.close
     books
   end
+
+  def read_people
+    people = []
+    return people unless File.exist?('./data/people.json')
+
+    file = File.open('./data/people.json')
+    data = JSON.parse(file.read)
+    data.each do |person|
+      if person['type'] == 'Student'
+        student = Student.new(person['classroom'], person['age'], name: person['name'],
+                                                                  parent_permission: person['has_permission'])
+        student.id = person['id']
+        people << student
+      else
+        teacher = Teacher.new(person['specialization'], person['age'], name: person['name'])
+        teacher.id = person['id']
+        people << teacher
+      end
+    end
+    file.close
+    people
+  end
