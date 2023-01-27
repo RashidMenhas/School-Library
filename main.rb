77c6f85e8a89432a2b72de3e50bdfb5fcfe0e4f4
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-27 16:20:16 +0500

    refactor code form main.rb file

--- main.rb
+++ main.rb
@@ -1,51 +1,10 @@
 require_relative './app'
-
-def menu
-  puts "\nPlease choose an option by entering a number:"
-  options = [
-    '1 - List all books',
-    '2 - List all people',
-    '3 - Create a person',
-    '4 - Create a book',
-    '5 - Create a rental',
-    '6 - List all rentals for a given person (id)',
-    '7 - Exit App'
-  ]
-  puts options
-end
-
-def option_case(choice)
-  case choice
-  when 1
-    list_all_books
-  when 2
-    list_all_people
-  when 3
-    create_person
-  when 4
-    create_book
-  when 5
-    create_rental
-  when 6
-    list_rentals_by_id
-  else
-    puts 'Incorrect choice, choose a number between 1..9'
-  end
-end
-
-def prompt_user
-  menu
-
-  choice = gets.chomp.to_i
-  choice == 7 ? quit_app : option_case(choice)
-
-  prompt_user
-end
+require './prompt_user'
 
 def main
   puts "\n👋 Welcome🙋 to School Library App!"
   app = App.new
-  app.run
+  prompt_user.new.prompt_user(app)
 end
 
 main
