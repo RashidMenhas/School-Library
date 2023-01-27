faa46378cce2e03ce8a3a8f9e52caff64f7131ab
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-27 19:56:13 +0100

    ADD read_books METHOD TO ReadData Class

--- read_data.rb
+++ read_data.rb
@@ -1,3 +1,14 @@
 class ReadData
+  def read_books
+    books = []
+    return books unless File.exist?('./data/books.json')
 
+    file = File.open('./data/books.json')
+    data = JSON.parse(file.read)
+    data.each do |book|
+      books << Book.new(book['title'], book['author'])
+    end
+    file.close
+    books
+  end
 end
