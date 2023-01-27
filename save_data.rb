433242df101aa2b5eee630f7084dd69d4144c7b6
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 01:43:04 +0500

    Make a method which create a folder if not exist in a root file and also create a file if not exists

--- save_data.rb
+++ save_data.rb
@@ -1,3 +1,11 @@
+require 'json'
+require 'fileutils'
+
 class SaveData
-  def check_file_exists(filename); end
+  def check_file_exists(filename)
+    FileUtils.mkdir_p('./data')
+    FileUtils.touch('./data/people.json') if !File.exist?('./data/people.json') && filename == 'people'
+    FileUtils.touch('./data/books.json') if !File.exist?('./data/books.json') && filename == 'books'
+    FileUtils.touch('./data/rentals.json') if !File.exist?('./data/rentals.json') && filename == 'rentals'
+  end
 end
