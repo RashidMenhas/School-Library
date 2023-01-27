3e9fbb753cfddebb241c9342917bf51005e2f848
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-27 23:00:20 +0100

    Add read_rentals and make_rental methods to ReadData Class

--- read_data.rb
+++ read_data.rb
@@ -35,3 +35,33 @@ class ReadData
     file.close
     people
   end
+
+  def read_rentals
+    rentals = []
+    return rentals unless File.exist?('./data/rentals.json')
+
+    file = File.open('./data/rentals.json')
+    data = JSON.parse(file.read)
+    data.each do |rental|
+      rentals << make_rental(rental)
+    end
+    file.close
+    rentals
+  end
+
+  def make_rental(rental)
+    if rental['type'] == 'Student'
+      student = Student.new(rental['classroom'], rental['age'], name: rental['name'],
+                                                                parent_permission: rental['has_permission'])
+      student.id = rental['id']
+      book = Book.new(rental['title'], rental['author'])
+      rental = Rental.new(rental['date'], student, book)
+    else
+      teacher = Teacher.new(rental['specialization'], rental['age'], name: rental['name'])
+      teacher.id = rental['id']
+      book = Book.new(rental['title'], rental['author'])
+      rental = Rental.new(rental['date'], teacher, book)
+    end
+    rental
+  end
+end
