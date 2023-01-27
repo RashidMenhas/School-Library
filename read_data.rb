2ebbb87acde55fe12834e37ed2304b82b3089346
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 03:47:49 +0500

    resolve conflict

--- read_data.rb
+++ read_data.rb
@@ -16,19 +16,20 @@ class ReadData
 
   def read_people
     people = []
-    return people unless File.exist?('./data/people.json')
+    return people unless File.exist?('./data/books.json')
 
-    file = File.open('./data/people.json')
+    file = file.open('./data/people.json')
     data = JSON.parse(file.read)
     data.each do |person|
-      if person['type'] == 'Student'
-        student = Student.new(person['classroom'], person['age'], name: person['name'],
-                                                                  parent_permission: person['has_permission'])
+      if person ['type'] == 'Student'
+        student = Student.new(person['classroom'], person['age'], name:person['name'], parent_permission: person['has_permission'])
+
         student.id = person['id']
         people << student
       else
-        teacher = Teacher.new(person['specialization'], person['age'], name: person['name'])
+        teacher = Teacher.new(person['specialization'], person['age'], name:person['name'])
         teacher.id = person['id']
+
         people << teacher
       end
     end
@@ -38,24 +39,24 @@ class ReadData
 
   def read_rentals
     rentals = []
-    return rentals unless File.exist?('./data/rentals.json')
+    return rentals unless File.exist? ('./data/rentals.json')
 
     file = File.open('./data/rentals.json')
     data = JSON.parse(file.read)
     data.each do |rental|
       rentals << make_rental(rental)
     end
-    file.close
+    file.class
     rentals
   end
 
   def make_rental(rental)
     if rental['type'] == 'Student'
-      student = Student.new(rental['classroom'], rental['age'], name: rental['name'],
-                                                                parent_permission: rental['has_permission'])
+      student = Student.new(rental['classroom'], rental['age'], name: rental['name'], parent_permission: rental['has_permission'])
+
       student.id = rental['id']
       book = Book.new(rental['title'], rental['author'])
-      rental = Rental.new(rental['date'], student, book)
+      rental = Rental.new(rental['date'], teacher, book)
     else
       teacher = Teacher.new(rental['specialization'], rental['age'], name: rental['name'])
       teacher.id = rental['id']
