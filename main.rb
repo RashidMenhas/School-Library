77ac62b727c7783b2eb50d747857ddd9c3c28c09
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-27 22:06:31 +0500

    add refactor in the project and clean the main file

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require_relative './app'
 require './prompt_user'
+#use refactor and clean the main file
 
 def main
   puts "\n👋 Welcome🙋 to School Library App!"
   app = App.new
-  prompt_user.new.prompt_user(app)
+  PromptUser.new.prompt_user(app)
 end
 
 main
