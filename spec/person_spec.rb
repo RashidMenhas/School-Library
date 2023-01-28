34963b4bf2da98c95dd0165200ad63f37b298a3a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-28 19:30:57 +0100

    ADD context for creating a new person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,4 +10,17 @@ describe Person do
     @book3 = Book.new('The Lord of the Rings', 'Tolkien')
     @rental = Rental.new(Time.now, @person1, @book1)
   end
+  context 'When creating a new Person' do
+    it 'should have the following attributes (id, age, name, and parent_permission, rentals)' do
+      expect(@person1.id).to be_between(1, 100).inclusive
+      expect(@person1.age).to be_kind_of Integer
+      expect(@person1.parent_permission).to be_kind_of FalseClass
+      expect(@person1.age).to eq(12)
+      expect(@person1.name).to eql('Little Smith')
+    end
+    it 'should be an instance of the class Person' do
+      expect(@person1).to be_instance_of Person
+    end
+  end
+
 end
