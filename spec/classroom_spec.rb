53c017c9faccaa95091f26a8c247a3a6e54679bb
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 21:16:23 +0500

    It's create new classroom and check if the label will be string, instance will be classroom, classroom.lable equal to CS50 and king of string

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -6,5 +6,16 @@ describe Classroom do
         @classroom = Classroom.new('CS50')
         @student = Student.new('B', 23, name: 'John')
       end
+      context 'When creating a new Classroom class' do
+        it 'should have a label property' do
+          expect(@classroom.label).to eql 'CS50'
+        end
+        it 'the label should be a string' do
+          expect(@classroom.label).to be_kind_of String
+        end
+        it 'should be an instance of the Classroom class' do
+          expect(@classroom).to be_instance_of Classroom
+        end
+      end
 
 end
