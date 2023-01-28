0b6447973966e6b0480d165f85ff030bbf0ed559
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 22:13:14 +0500

    Describe TrimmerDecorator and perform all the functionality of the unit test method

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../person'
-require_relative '../capitalize_decorator'
-require_relative '../trimmer_decorator'
+require_relative '../capitalizedecorator'
+require_relative '../trimmerdecorator'
 
 describe CapitalizeDecorator do
     before :each do
@@ -20,3 +20,24 @@ describe CapitalizeDecorator do
         end
       end
 end
+
+describe TrimmerDecorator do
+    before :each do
+      @person = Person.new(22, name: 'maximilianus')
+      @capitalized_name = CapitalizeDecorator.new(@person)
+      @trimmed_name = TrimmerDecorator.new(@person)
+    end
+  
+    context 'When using the correct_name method from the TrimmerDecorator class' do
+      it 'can use correct_name' do
+        expect(@trimmed_name).to respond_to(:correct_name)
+      end
+      it 'should have length of 10' do
+        expect(@trimmed_name.correct_name.length).to eq 10
+      end
+      it 'should trim the name if it is longer than 10 characters' do
+        expect(@trimmed_name.correct_name).to eql 'maximilian'
+      end
+    end
+  end
+  
