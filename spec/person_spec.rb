b10bb8f1c6f69616c55bd8a05777be9ac1279b6f
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-28 19:29:58 +0100

    ADD method to Instantiate new Person for each loop

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,5 +2,12 @@ require_relative '../person'
 require_relative '../book'
 require_relative '../rental'
 describe Person do
-
+  before :each do
+    @person1 = Person.new(12, name: 'Little Smith', parent_permission: false)
+    @person2 = Person.new(500, name: 'Elrond HalfElf')
+    @book1 = Book.new('Game of Thrones', 'George Martin')
+    @book2 = Book.new('The Songs of Achilles', 'Madelaine Miller')
+    @book3 = Book.new('The Lord of the Rings', 'Tolkien')
+    @rental = Rental.new(Time.now, @person1, @book1)
+  end
 end
