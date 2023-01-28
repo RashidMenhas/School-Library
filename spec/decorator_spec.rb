a1f1b96b9646e95f8c891e2f9c4c41e9ff67abd7
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-28 22:25:53 +0500

    Describe nameable and it's arise_error NotImplementedError

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -3,41 +3,40 @@ require_relative '../capitalizedecorator'
 require_relative '../trimmerdecorator'
 
 describe CapitalizeDecorator do
-    before :each do
-        @person = Person.new(22, name: 'maximilianus')
+  before :each do
+    @person = Person.new(22, name: 'maximilianus')
     @capitalized_name = CapitalizeDecorator.new(@person)
-    end
+  end
 
-    context 'When using the correct_name method from the CapitalizeDecorator class' do
-        it 'can use correct_name' do
-          expect(@capitalized_name).to respond_to(:correct_name)
-        end
-        it 'should have the first letter as uppercase' do
-          expect(@capitalized_name.correct_name[0]).to eql @person.name[0].upcase
-        end
-        it 'should return the name in capitalized' do
-          expect(@capitalized_name.correct_name).to eql 'Maximilianus'
-        end
-      end
+  context 'When using the correct_name method from the CapitalizeDecorator class' do
+    it 'can use correct_name' do
+      expect(@capitalized_name).to respond_to(:correct_name)
+    end
+    it 'should have the first letter as uppercase' do
+      expect(@capitalized_name.correct_name[0]).to eql @person.name[0].upcase
+    end
+    it 'should return the name in capitalized' do
+      expect(@capitalized_name.correct_name).to eql 'Maximilianus'
+    end
+  end
 end
 
 describe TrimmerDecorator do
-    before :each do
-      @person = Person.new(22, name: 'maximilianus')
-      @capitalized_name = CapitalizeDecorator.new(@person)
-      @trimmed_name = TrimmerDecorator.new(@person)
+  before :each do
+    @person = Person.new(22, name: 'maximilianus')
+    @capitalized_name = CapitalizeDecorator.new(@person)
+    @trimmed_name = TrimmerDecorator.new(@person)
+  end
+
+  context 'When using the correct_name method from the TrimmerDecorator class' do
+    it 'can use correct_name' do
+      expect(@trimmed_name).to respond_to(:correct_name)
     end
-  
-    context 'When using the correct_name method from the TrimmerDecorator class' do
-      it 'can use correct_name' do
-        expect(@trimmed_name).to respond_to(:correct_name)
-      end
-      it 'should have length of 10' do
-        expect(@trimmed_name.correct_name.length).to eq 10
-      end
-      it 'should trim the name if it is longer than 10 characters' do
-        expect(@trimmed_name.correct_name).to eql 'maximilian'
-      end
+    it 'should have length of 10' do
+      expect(@trimmed_name.correct_name.length).to eq 10
+    end
+    it 'should trim the name if it is longer than 10 characters' do
+      expect(@trimmed_name.correct_name).to eql 'maximilian'
     end
   end
-  
+end
--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -1,5 +1,28 @@
 require_relative '../person'
 require_relative '../nameable'
 
+describe Nameable do
+  before :each do
+    @person = Person.new(39, name: 'Obi Wan Kenobi')
+    @nameable = Nameable.new
+  end
 
+  context 'When creating a Nameable instance' do
+    it 'should raise NotImplementedError' do
+      expect do
+        @nameable.correct_name
+      end.to raise_error('Nameable has not implemented method \'correct_name\'', NotImplementedError)
+    end
+  end
 
+  context 'When calling correct_name on Person instance' do
+    it 'should not raise error an error since it\'s implemented' do
+      expect do
+        @person.correct_name
+      end.not_to raise_error
+    end
+    it 'should retrun the name attribute correctly' do
+      expect(@person.correct_name).to eql 'Obi Wan Kenobi'
+    end
+  end
+end
